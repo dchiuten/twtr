7cd0ee5441abf78330de97add69de0c3206f2377
Dennis Chiuten <dchiuten@twitter.com>
2021-01-28 09:14:42 +0800

    updates to reflect new Twitter brand guidelines and fix for yq_breaks function

--- twViz/R/twPalette.R
+++ twViz/R/twPalette.R
@@ -1,27 +1,44 @@
 twPalette <- c(
-  "#1DA1F2", #dark blue
-  "#794BC4", #dark purple
-  "#E0245E", #dark pink
-  "#FFAD1F", #dark yellow
-  "#F45D22", #dark orange
-  "#17BF63", #dark green
-  "#71C9F8", #light blue
-  "#A37CED", #light purple
-  "#FF8D57", #light orange
-  "#FFD03F", #light yellow
-  "#F6809A", #light pink
-  "#68E090", #light green
-  "#005FD1", #extra dark blue
-  "#4F0299", #extra dark purple
-  "#A01744", #extra dark pink
-  "#D82E18", #extra dark orange
-  "#F98E00", #extra dark yellow
-  "#008951", #extra dark green
-  "#97E3FF", #extra light blue
-  "#C7B4FA", #extra light purple
-  "#FFB8C2", #extra light pink
-  "#FFBE78", #extra light orange
-  "#FFE76E", #extra light yellow
-  "#A5F2AA"  #extra light green
+  "#1D9BF0", #blue
+  "#FCD61A", #vibrantyellow
+  "#ED0277", #vibrantmagenta
+  "#FA4233", #vibrantred
+  "#7366D8", #vibrantpurple
+  "#23C59E", #vibrantgreen
+  "#97E3FF", #lightblue
+  "#FFF094", #lightyellow
+  "#FF7D63", #lightred
+  "#AAB9FF", #lightpurple
+  "#F2912E", #vibrantorange
+  "#295C47", #darkgreen
+  "#965A28", #darkorange
+  "#61263B", #darkmagenta
+  "#024E9A", #darkblue
+  "#CFD9DE" #grey
+
+  # "#1DA1F2", #dark blue
+  # "#794BC4", #dark purple
+  # "#E0245E", #dark pink
+  # "#FFAD1F", #dark yellow
+  # "#F45D22", #dark orange
+  # "#17BF63", #dark green
+  # "#71C9F8", #light blue
+  # "#A37CED", #light purple
+  # "#FF8D57", #light orange
+  # "#FFD03F", #light yellow
+  # "#F6809A", #light pink
+  # "#68E090", #light green
+  # "#005FD1", #extra dark blue
+  # "#4F0299", #extra dark purple
+  # "#A01744", #extra dark pink
+  # "#D82E18", #extra dark orange
+  # "#F98E00", #extra dark yellow
+  # "#008951", #extra dark green
+  # "#97E3FF", #extra light blue
+  # "#C7B4FA", #extra light purple
+  # "#FFB8C2", #extra light pink
+  # "#FFBE78", #extra light orange
+  # "#FFE76E", #extra light yellow
+  # "#A5F2AA"  #extra light green
 )
 
--- twViz/R/yq_breaks.R
+++ twViz/R/yq_breaks.R
@@ -1,6 +1,6 @@
 yq_breaks <- function(data, yq_col = yq) {
   df <- data %>%
     dplyr::rename(yq = {{yq_col}})
-  sequence <- seq(min(data$yq), max(data$yq), by = 0.25)
+  sequence <- seq(min(df$yq), max(df$yq), by = 0.25)
   return(sequence)
 }
