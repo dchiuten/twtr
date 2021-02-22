aff8dd334d028a18f04db114c1980cc92375bec4
Dennis Chiuten <dchiuten@twitter.com>
2021-02-22 17:50:07 +0800

    updated twPalette to named color vector

--- twViz/R/twPalette.R
+++ twViz/R/twPalette.R
@@ -1,44 +1,19 @@
 twPalette <- c(
-  "#1D9BF0", #blue
-  "#ED0277", #vibrantmagenta
-  "#FCD61A", #vibrantyellow
-  "#23C59E", #vibrantgreen
-  "#7366D8", #vibrantpurple
-  "#FA4233", #vibrantred
-  "#97E3FF", #lightblue
-  "#FFF094", #lightyellow
-  "#FF7D63", #lightred
-  "#AAB9FF", #lightpurple
-  "#F2912E", #vibrantorange
-  "#295C47", #darkgreen
-  "#965A28", #darkorange
-  "#61263B", #darkmagenta
-  "#024E9A", #darkblue
-  "#CFD9DE" #grey
-
-  # "#1DA1F2", #dark blue
-  # "#794BC4", #dark purple
-  # "#E0245E", #dark pink
-  # "#FFAD1F", #dark yellow
-  # "#F45D22", #dark orange
-  # "#17BF63", #dark green
-  # "#71C9F8", #light blue
-  # "#A37CED", #light purple
-  # "#FF8D57", #light orange
-  # "#FFD03F", #light yellow
-  # "#F6809A", #light pink
-  # "#68E090", #light green
-  # "#005FD1", #extra dark blue
-  # "#4F0299", #extra dark purple
-  # "#A01744", #extra dark pink
-  # "#D82E18", #extra dark orange
-  # "#F98E00", #extra dark yellow
-  # "#008951", #extra dark green
-  # "#97E3FF", #extra light blue
-  # "#C7B4FA", #extra light purple
-  # "#FFB8C2", #extra light pink
-  # "#FFBE78", #extra light orange
-  # "#FFE76E", #extra light yellow
-  # "#A5F2AA"  #extra light green
-)
+  "blue" = "#1D9BF0",
+  "vibrantmagenta" = "#ED0277",
+  "vibrantyellow" = "#FCD61A",
+  "vibrantgreen" = "#23C59E",
+  "vibrantpurple" = "#7366D8",
+  "vibrantred" = "#FA4233",
+  "lightblue" = "#97E3FF",
+  "lightyellow" = "#FFF094",
+  "lightred" = "#FF7D63",
+  "lightpurple" = "#AAB9FF",
+  "vibrantorange" = "#F2912E",
+  "darkgreen" = "#295C47",
+  "darkorange" = "#965A28",
+  "darkmagenta" = "#61263B",
+  "darkblue" = "#024E9A",
+  "grey" = "#CFD9DE"
+  )
 
